ba9ce72a55f780171b2f22c4f99299cb51d7677f
Radul19 <radulito19@gmail.com>
2023-12-08 21:13:01 -0400

    url

--- src/api/url.js
+++ src/api/url.js
@@ -1,6 +1,5 @@
 export const url = "https://norep-back.vercel.app/" 
 // export const url = "http://192.168.1.111:4000/" 
-// export const url = "https://norep-kswpwnwdk-noreps-projects.vercel.app/" 
 
 
 /// Funcion general para el catch error que se usa en TODAS las peticiones 
