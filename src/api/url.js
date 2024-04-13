120f285a2d4da1eb55e04113fa73670e9909fb4e
Radul19 <radulito19@gmail.com>
2024-04-12 23:01:45 -0400

    url fix

--- src/api/url.js
+++ src/api/url.js
@@ -1,6 +1,6 @@
 // export const url = "https://norep-back.vercel.app/" 
-export const url = "http://192.168.1.114:4000/" 
-// export const url = "https://testback.norep.com.ve/" 
+// export const url = "http://192.168.1.114:4000/" 
+export const url = "https://testback.norep.com.ve/" 
 
 
 /// Funcion general para el catch error que se usa en TODAS las peticiones 
