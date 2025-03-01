1550740ebe9c6c63cee78096d470ba6793627a33
Radul19 <radulito19@gmail.com>
2025-02-28 21:09:28 -0400

    fixed calendar

--- src/views/Guest/Home/Calendar.tsx
+++ src/views/Guest/Home/Calendar.tsx
@@ -1,120 +1,134 @@
 // import { useContext } from "react";
+import { useContext } from "react";
+import { convertDate, months, todaySplit } from "../../../helpers/date";
 import { EvnFields } from "../../../types/event";
 import "./calendar.sass";
+import Context from "../../../helpers/UserContext";
+import { HashLink } from "react-router-hash-link";
 // import { convertDate, months, todaySplit } from "../../../helpers/date";
 // import Context from "../../../helpers/UserContext";
 // import { HashLink } from "react-router-hash-link";
 
-// const thisMonth = new Date().getUTCDate()+1
-// const thisYear = new Date().getUTCFullYear()
-// const minus = (n: number) => {
-//   let baseNum = 0;
-//   if (thisMonth < 3 && n < 0) baseNum = 12;
-//   let result = baseNum + n;
-//   return result;
-// };
+const thisMonth = new Date().getUTCDate() + 1;
+const thisYear = new Date().getUTCFullYear();
+const minus = (n: number) => {
+  let baseNum = 0;
+  if (thisMonth < 3 && n < 0) baseNum = 12;
+  let result = (baseNum + n + 1).toString();
+  if (result.length < 2) result = `0${result}`;
+  return `${thisYear}-${result}`;
+};
 
-// const monthsArr = [
-//   minus(-3),
-//   minus(-2),
-//   minus(-1),
-//   minus(0),
-//   minus(1),
-//   minus(2),
-// ];
-
+const monthsArr = [
+  minus(-2),
+  minus(-1),
+  minus(0),
+  minus(1),
+  minus(2),
+  minus(3),
+];
 // const getMonth = (d: string) => {
 // };
 
 export const Calendar = ({ events }: { events?: EvnFields[] }) => {
   if (!events) return null;
 
-  // monthsArr.map((month, index) => {
-
+  // monthsArr.map((isDate, index) => {
+  //   events.forEach(e => {
+  //     const eDate = todaySplit(e.since)
+  //     // console.log(isDate);
+  //     // console.log(`${eDate[0]}-${eDate[1]}`,e.name);
+  //     if(isDate === `${eDate[0]}-${eDate[1]}`) console.log('bingo');
+  //   });
   // });
 
-  // events.forEach((e)=>{
-  //   const eDate = todaySplit(e.since)
-  //   console.log(eDate[0],eDate[1]);
-  //   if()
-  // })
+  events.forEach((e) => {
+    const eDate = todaySplit(e.since);
+    if (eDate[0] === `${thisYear}`) console.log(e.name);
+  });
 
   return (
     <div className="calendar" id="Calendar">
       <h6 className="title">CALENDARIO</h6>
       <p className="subtitle">Competencias que cubrimos</p>
-      {/* <div className="calendar_ctn">
-        {monthsArr.map((month, index) => {
-          const monthsEvents = events.filter(
-            (e: any) => getMonth(e.since) === month
-          );
+      <div className="calendar_ctn">
+        {monthsArr.map((isDate, index) => {
+          const monthsEvents = events.filter((e: any) => {
+            const eDate = todaySplit(e.since);
+            if (isDate === `${eDate[0]}-${eDate[1]}`) return true;
+            else return false;
+          });
           return (
-            <CalendarRow month={month} key={index} events={monthsEvents} />
+            <CalendarRow
+              month={parseInt(isDate.split("-")[1]) - 1}
+              key={index}
+              events={monthsEvents}
+            />
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 };
-// const CalendarRow = ({
-//   month,
-//   events,
-// }: {
-//   month: number;
-//   events: EvnFields[];
-// }) => {
-//   return (
-//     <div className="calendar_row">
-//       <h6> {months[month]?.toUpperCase() ?? ""}</h6>
-//       <hr />
-//       <div className="calendar_cards_ctn">
-//         {events.map((e) => (
-//           <CalendarCard key={e._id} event={e} />
-//         ))}
-//         {/* <div className="test"></div> */}
-//       </div>
-//     </div>
-//   );
-// };
+const CalendarRow = ({
+  month,
+  events,
+}: {
+  month: number;
+  events: EvnFields[];
+}) => {
+  return (
+    <div className="calendar_row">
+      <h6> {months[month]?.toUpperCase() ?? ""}</h6>
+      <hr />
+      <div className="calendar_cards_ctn">
+        {events.map((e) => (
+          <CalendarCard key={e._id} event={e} />
+        ))}
+        {/* <div className="test"></div> */}
+      </div>
+    </div>
+  );
+};
 
-// const CalendarCard = ({ event }: { event: EvnFields }) => {
-//   const { adminData } = useContext(Context);
-//   const CalendarCardInfo = () => {
-//     return (
-//       <>
-//         <div className="cc_top">
-//           <h1 className="cc_title">{event.name}</h1>
-//           <h1 className="cc_categ">{event.categories[0]?.name}</h1>
-//           {event.categories.length > 1 && (
-//             <h1 className="cc_categ">{event.categories[1]?.name}</h1>
-//           )}
-//           {/* <h1 className="cc_categ">{event.categories[1]?.name}</h1> */}
-//         </div>
-//         <div className="cc_bot">
-//           <h1>Ver Tabla</h1>
-//           <p className="cc_date">
-//             {convertDate(event.since)} - {convertDate(event.until)}
-//           </p>
-//         </div>
-//       </>
-//     );
-//   };
+const CalendarCard = ({ event }: { event: EvnFields }) => {
+  const { adminData } = useContext(Context);
+  const CalendarCardInfo = () => {
+    return (
+      <>
+        <div className="cc_top">
+          <h1 className="cc_title">{event.name}</h1>
+          <h1 className="cc_categ">{event.categories[0]?.name}</h1>
+          {event.categories.length > 1 && (
+            <h1 className="cc_categ">{event.categories[1]?.name}</h1>
+          )}
+          {/* <h1 className="cc_categ">{event.categories[1]?.name}</h1> */}
+        </div>
+        <div className="cc_bot">
+          <h1>Ver Tabla</h1>
+          <p className="cc_date">
+            {convertDate(event.since)} - {convertDate(event.until)}
+          </p>
+        </div>
+      </>
+    );
+  };
 
-//   return (
-//     <>
-//       {event.accesible || adminData ? (
-//         //@ts-ignore
-//         <HashLink
-//           className="calendar_card"
-//           to={`/resultados/${event?._id}#top`}
-//         >
-//           <CalendarCardInfo />
-//         </HashLink>
-//       ) : (
-//         <div className="calendar_card">
-//           <CalendarCardInfo />
-//         </div>
-//       )}
-//     </>
-//   );
-// };
+  return (
+    <>
+      {event.accesible || adminData ? (
+        //@ts-ignore
+        <HashLink
+          className="calendar_card"
+          to={`/resultados/${event?._id}#top`}
+        >
+          <CalendarCardInfo />
+        </HashLink>
+      ) : (
+        <div className="calendar_card">
+          <CalendarCardInfo />
+        </div>
+      )}
+    </>
+  );
+};
